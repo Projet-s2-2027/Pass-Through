9d70832f0ce44460d5b1ae90f41be6f7310efb41
Thomas Baller <thomas.baller@epita.fr.com>
2023-02-07 22:06:36 +0100

    Deplacement fait mais bug

--- Pass Through/Assets/Script/PlayerController.cs
+++ Pass Through/Assets/Script/PlayerController.cs
@@ -6,15 +6,19 @@ public class PlayerController : MonoBehaviour
 {
    //vitesse du joueur
    [SerializeField]
-   private float speed;
+   private float speed = 3f;
+   [SerializeField]
+   private float mouseSensitivityX = 3f;
+   [SerializeField]
+   private float mouseSensitivityY = 3f;
    
    //récupère les scripts du player motor
-   private playerMotor _motor;
+   private playerMotor motor;
 
    private void Start()
    {
       //constructeur du motor
-      _motor = GetComponent<playerMotor>();
+      motor = GetComponent<playerMotor>();
    }
 
    private void Update()
@@ -27,5 +31,24 @@ public class PlayerController : MonoBehaviour
       Vector3 moveVertical = transform.forward * zMov;
 
       Vector3 velocity = (moveHorizontal + moveVertical).normalized * speed;
+      
+	   motor.Move(velocity);
+      
+      //On calcule la rotation du joueur en Vector3
+      float yRot = Input.GetAxisRaw("Mouse X");
+      
+      Vector3 rotation = new Vector3(0,yRot,0) * mouseSensitivityX;
+
+      motor.Rotate(rotation);
+      //On calcule la rotation du joueur en Vector3
+      float xRot = Input.GetAxisRaw("Mouse Y");
+      
+      Vector3 cameraRotation = new Vector3(xRot,0,0) * mouseSensitivityY;
+
+      motor.RotateCamera(cameraRotation);
+      
+
+
+
    }
 }
