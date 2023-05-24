55f3f6d9376b199ca93a1a8cab973ac6a30953be
Martin <aypierro650@gmail.com>
2023-05-24 13:10:22 +0200

    interupteur, fabrice vas etre content

--- Pass Through/Assets/Script/LightController.cs
+++ Pass Through/Assets/Script/LightController.cs
@@ -6,7 +6,10 @@ public class LightController : MonoBehaviour
 {
     public KeyCode interactKey = KeyCode.E;
     public GameObject button;
-    public Light targetLight;
+    public Light targetLight1;
+    public Light targetLight2;
+    public Light targetLight3;
+    public Light targetLight4;
 
     private bool isLookingAtButton = false;
     private bool isLightEnabled = true;
@@ -25,24 +28,33 @@ public class LightController : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject == button)
-        {
-            isLookingAtButton = true;
-        }
+        isLookingAtButton = true;  
     }
 
-    void OnTriggerExit(Collider other)
+    void OnTriggerExit()
     {
-        if (other.gameObject == button)
-        {
-            isLookingAtButton = false;
-        }
+        isLookingAtButton = false;
     }
 
     void ToggleLight()
     {
         isLightEnabled = !isLightEnabled;
-        targetLight.enabled = isLightEnabled;
+        if (targetLight1 != null)
+        {
+            targetLight1.enabled = isLightEnabled;
+        }
+        if (targetLight2 != null)
+        {
+            targetLight2.enabled = isLightEnabled;
+        }
+        if (targetLight3 != null)
+        {
+            targetLight3.enabled = isLightEnabled;
+        }
+        if (targetLight4 != null)
+        {
+            targetLight4.enabled = isLightEnabled;
+        }
         isState1Active = !isState1Active;
         state1.SetActive(isState1Active);
         state2.SetActive(!isState1Active);
