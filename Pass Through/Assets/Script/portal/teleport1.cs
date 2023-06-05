52cc7b11617eafddf6903a98ec1f9220c5e57340
Thomas Baller <thomas.baller@epita.fr.com>
2023-06-05 21:04:46 +0200

    ajout de portail

---
+++ Pass Through/Assets/Script/portal/teleport1.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teleport1 : MonoBehaviour
+{ 
+   Vector3 destination;
+
+   private void OnCollisionEnter(Collision col)
+   {
+      if (this.name=="portal3")
+      {
+         destination = GameObject.Find("portal4").transform.position;
+      }
+      else
+      {
+         destination = GameObject.Find("portal3").transform.position;
+      }
+
+      col.transform.position = destination+Vector3.forward *2;
+      col.transform.Rotate(Vector3.up*180);
+   }
+}
